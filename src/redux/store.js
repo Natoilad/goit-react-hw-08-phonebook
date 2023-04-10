8ab72794d7d5dfe34c21394cdbbe19da34009945
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-04-10 03:44:00 +0300

    pers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,48 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './sliceContact';
 import { filterReducer } from './sliceFilter';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'token',
+  storage,
+  whiteList: ['token'],
+};
+
+const persistReducerUser = persistReducer(persistConfig);
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    auth: persistReducerUser,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistR = persistStore(store);
+
+// import { sliceFilter } from './sliceFilter';
+
+// import { sliceContact } from './sliceContact';
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
